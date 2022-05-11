20e00c42f0c1950c9825e9b4d3f86619a436ed86
dav12id3 <dav12id3@gmail.com>
2022-05-11 21:33:28 +0800

    plots and codes

---
+++ plot1.R
@@ -0,0 +1,7 @@
+library(dplyr)
+library(lubridate)
+data <- read.table("household_power_consumption.txt", header = TRUE, sep = ";", na.strings = "?")
+data <- filter(data, Date == "1/2/2007" | Date == "2/2/2007")
+hist(data$Global_active_power, xlab = "Global Active Power (kilowatts)", col = "red", main = "Global Active Power", xaxp = c(0,6,3), yaxp = c(0,1200,6))
+dev.copy(png, file = "plot1.png")
+dev.off()
